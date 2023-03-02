e39d832299fcd8a0c53979df96a8c5d7dff80a35
ricarthlima <rrsl@cin.ufpe.br>
2023-03-02 10:50:32 -0300

    v5.1 - firebase auth x firebase firestore

--- lib/firestore/services/listin_service.dart
+++ lib/firestore/services/listin_service.dart
@@ -1,18 +1,20 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_firebase_authentication/firestore/models/listin.dart';
 
 class ListinService {
+  String uid = FirebaseAuth.instance.currentUser!.uid;
   FirebaseFirestore firestore = FirebaseFirestore.instance;
 
   Future<void> adicionarListin({required Listin listin}) async {
-    return firestore.collection("listins").doc(listin.id).set(listin.toMap());
+    return firestore.collection(uid).doc(listin.id).set(listin.toMap());
   }
 
   Future<List<Listin>> lerListins() async {
     List<Listin> temp = [];
 
     QuerySnapshot<Map<String, dynamic>> snapshot =
-        await firestore.collection("listins").get();
+        await firestore.collection(uid).get();
 
     for (var doc in snapshot.docs) {
       temp.add(Listin.fromMap(doc.data()));
@@ -22,6 +24,6 @@ class ListinService {
   }
 
   Future<void> removerListin({required String listinId}) async {
-    return firestore.collection('listins').doc(listinId).delete();
+    return firestore.collection(uid).doc(listinId).delete();
   }
 }
